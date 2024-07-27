47dca356bc01835749a56c8e8ce974d03284b9bb
Tolga Yavuz <itstolga@gmail.com>
2024-07-27 07:32:01 +0300

    Disabling bias input for 4 secs. after sending it to BD.

--- frontend/src/app/components/bias-input/bias-input.component.ts
+++ frontend/src/app/components/bias-input/bias-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as apiConfig from '../../../assets/apiConfig.json';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-bias-input',
@@ -9,12 +10,17 @@ import * as apiConfig from '../../../assets/apiConfig.json';
 })
 export class BiasInputComponent {
   biasCharacter: string = '';
+  disableInput: boolean = false;
 
   constructor(private http: HttpClient) {}
 
   updateBias() {
     if (this.biasCharacter.match(/[a-z]/)) {
       this.http.post(apiConfig.setBias.url, { bias: this.biasCharacter.toLowerCase() }).subscribe();
+      this.disableInput = true;
+      interval(4000).subscribe(() => {
+        this.disableInput = false;
+      })
     }
   }
 }
